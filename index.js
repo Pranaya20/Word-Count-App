20b00d97742c3853780b97b17a158983bf49fe0b
Pranaya <patropranaya23@gmail.com>
2023-01-12 22:37:44 +0530

    created the js file

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
-const textData = document.getElementById('text-area');
-function onchange(e){
-    textData.innerHTML = e.target.value;
-    console.log(textData.innerHTML);
-}
-textData.addEventListener('textarea', onchange);
+const textbox = document.getElementById('text-box');
+console.log("textData",textbox);
+
+textbox.addEventListener('input',function(){
+   let text = this.value;
+   const textlength = text.length;
+   document.getElementById("char").innerHTML = textlength; 
+   text = text.trim();
+   wordCount = text.split(" ");
+   let cleanList = wordCount.filter(function(element){
+    return element != "";
+   })
+   document.getElementById("word").innerHTML = cleanList.length; 
+
+   let line = text.split('.');
+   document.getElementById("sentence").innerHTML = line.length; 
+   let para = text.split(/\n/);
+   document.getElementById("paragraph").innerHTML = para.length;   
+});
+
